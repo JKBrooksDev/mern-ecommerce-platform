ca0e4f0e5843e6a5e98cbe700f24324305695e84
Jason Brooks <jasonbrooks1127@gmail.com>
2025-12-22 15:05:26 -0500

    Fix case-sensitive import for AdminOrders

--- frontend/src/pages/AdminOrdersPage.jsx
+++ frontend/src/pages/AdminOrdersPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import { AdminOrders } from '../features/admin/components/AdminOrders'
-import { AdminOrders } from '../features/admin/components/adminOrders'
+import { AdminOrders } from '../features/admin/components/AdminOrders'
+// import { AdminOrders } from '../features/admin/components/adminOrders'
 import {Navbar} from '../features/navigation/components/Navbar'
 
 export const AdminOrdersPage = () => {
