0c18bcb545aa9ffa89548a77499b6b5c3162bdae
Jason Brooks <jasonbrooks1127@gmail.com>
2025-12-22 18:02:08 -0500

    Use VITE_API_URL instead of localhost

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,3 +1,9 @@
+// import axios from 'axios'
+
+// export const axiosi=axios.create({withCredentials:true,baseURL:"http://localhost:8000"})
 import axios from 'axios'
 
-export const axiosi=axios.create({withCredentials:true,baseURL:"http://localhost:8000"})
+export const axiosi = axios.create({
+  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL
+})
