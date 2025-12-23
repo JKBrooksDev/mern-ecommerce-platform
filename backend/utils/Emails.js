c6a125d4eff503394aa15fd480f4614430ae04a3
Jason Brooks <jasonbrooks1127@gmail.com>
2025-12-22 20:32:40 -0500

    Switched email service to Resend

--- backend/utils/Emails.js
+++ backend/utils/Emails.js
@@ -1,18 +1,31 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.PASSWORD,
+//   },
+// });
 
-exports.sendMail = async(receiverEmail,subject,body) => {
-    await transporter.sendMail({
-    from: process.env.EMAIL,
+// exports.sendMail = async(receiverEmail,subject,body) => {
+//     await transporter.sendMail({
+//     from: process.env.EMAIL,
+//     to: receiverEmail,
+//     subject: subject,
+//     html: body
+//   });
+// };
+
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+exports.sendMail = async (receiverEmail, subject, body) => {
+  await resend.emails.send({
+    from: "Your App <onboarding@resend.dev>",
     to: receiverEmail,
-    subject: subject,
-    html: body
+    subject,
+    html: body,
   });
 };
