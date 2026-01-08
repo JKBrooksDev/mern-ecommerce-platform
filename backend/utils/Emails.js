55ba8a2c37931ffdb32eb95d01d24926a2fbc98f
Jason Brooks <jasonbrooks1127@gmail.com>
2026-01-08 09:14:00 -0500

    Switch email system to Nodemailer + Outlook

--- backend/utils/Emails.js
+++ backend/utils/Emails.js
@@ -1,26 +1,25 @@
-const axios = require("axios");
+const nodemailer = require("nodemailer");
 
-exports.sendMail = async (receiverEmail, subject, body) => {
+exports.sendMail = async (to, subject, html) => {
   try {
-    const serviceId = process.env.EMAILJS_SERVICE_ID;
-    const templateId = process.env.EMAILJS_TEMPLATE_ID;
-    const publicKey = process.env.EMAILJS_PUBLIC_KEY;
-
-    const templateParams = {
-      to_email: receiverEmail,
-      subject: subject,
-      message: body,
-    };
-
-    await axios.post("https://api.emailjs.com/api/v1.0/email/send", {
-      service_id: serviceId,
-      template_id: templateId,
-      user_id: publicKey,
-      template_params: templateParams,
+    const transporter = nodemailer.createTransport({
+      service: "hotmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
     });
 
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    });
+
+    return true;
   } catch (error) {
-    console.error("EmailJS sendMail error:", error.response?.data || error);
+    console.log("Nodemailer error:", error);
     throw new Error("Failed to send email");
   }
 };
