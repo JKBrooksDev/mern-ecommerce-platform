16032e31873ea4ba5265b338fd2d4a893e658ecc
Jason Brooks <jasonbrooks1127@gmail.com>
2026-01-08 09:57:52 -0500

    Switch email system to Brevo SMTP

--- backend/utils/Emails.js
+++ backend/utils/Emails.js
@@ -3,7 +3,9 @@ const nodemailer = require("nodemailer");
 exports.sendMail = async (to, subject, html) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "hotmail",
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
